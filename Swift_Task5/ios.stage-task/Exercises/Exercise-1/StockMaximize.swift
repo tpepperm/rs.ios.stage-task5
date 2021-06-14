43e3a2d1a9512fe50328cbcbfc87c9756793ec0a
tpepperm <tkarpukova@gmail.com>
2021-06-14 21:28:00 +0300

    feat: task 5

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,14 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        guard prices.count > 0 else { return 0 }
+
+        var prices = prices
+        var profit = 0
+        while prices.count > 0 {
+            profit += prices.max()! - prices[0]
+            prices.remove(at: 0)
+        }
+        return profit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,60 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        guard message.count >= 1 && message.count <= 60,
+              message.range(of: "^[a-zA-Z]+\\[][0-9]+$", options: .regularExpression) == nil
+        else { return "" }
+
+        var message = message
+        while message.contains("[") || message.contains("]") {
+            decrypt(&message)
+        }
+        return message
+    }
+
+    func decrypt(_ message: inout String) {
+        guard let lastBracket = message.firstIndex(of: "]"),
+              let firstBracket = message[...lastBracket].lastIndex(of: "["),
+              firstBracket != message.startIndex
+        else {
+            message = message.replacingOccurrences(of: "[", with: "")
+            message = message.replacingOccurrences(of: "]", with: "")
+            return
+        }
+
+        var firstNumber: String.Index?
+        var lastNumber: String.Index?
+        if firstBracket > message.startIndex,
+           lastBracket > message.startIndex {
+            firstNumber = message.index(before: firstBracket)
+            lastNumber = message.index(before: firstBracket)
+        }
+
+        if var first = firstNumber,
+           let lastNumber = lastNumber,
+           message[first].isNumber,
+           message[lastNumber].isNumber {
+            while message[first].isNumber && first > message.startIndex {
+                first = message.index(before: first)
+            }
+            if first != firstNumber {
+                firstNumber = message.index(after: first)
+            }
+        }
+
+        let start = message.index(after: firstBracket)
+        let end = message.index(before: lastBracket)
+        let string = String(message[start...end])
+        if  let firstNumber = firstNumber,
+            let lastNumber = lastNumber,
+            let occurences = Int(message[firstNumber...lastNumber]) {
+            if occurences < 1 || occurences > 300 {
+                message = ""
+                return
+            }
+            message.replaceSubrange(firstNumber...lastBracket, with: String(repeating: string, count: occurences))
+        } else {
+            message.replaceSubrange(firstBracket...lastBracket, with: String(repeating: string, count: 1))
+        }
     }
 }
